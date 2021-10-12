1e0017a8c51c35d85472fc586b8afe46fe2ac995
SonicDarkSly <courteille.michael@gmail.com>
2021-10-12 15:13:42 +0200

    modif database connect

--- models/dbconfig.js
+++ models/dbconfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://admin:test@cluster0.dzq65.mongodb.net/database?retryWrites=true&w=majority";
+const uri = "mongodb+srv://user:passuser@cluster0.dzq65.mongodb.net/database?retryWrites=true&w=majority";
 const client = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(
